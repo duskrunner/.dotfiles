e61eac1835d85b5fbfad346b7785d6fea30eed8a
Stanislav Selin <stasvselin@gmail.com>
2023-06-22 21:23:42 +0300

    lua formating

--- nvchad/.config/nvim/lua/custom/plugins.lua
+++ nvchad/.config/nvim/lua/custom/plugins.lua
@@ -1,42 +1,40 @@
 local plugins = {
-  {
-    "christoomey/vim-tmux-navigator",
-    lasy = false,
-  },
-  {
-    "williamboman/mason.nvim",
-    opts = {
-      ensure_installed = {
-      "dockerfile-language-server",
-      "eslint_d",
-      "lua-language-server",
-      "prettierd",
-      "typescript-language-server",
-      "clangd",
-      "clang-format",
-      "gopls",
-      "gofumpt",
-      "goimports-reviser",
-      "golines",
-      "rust-analyzer",
-      "rustfmt"
-      }
-    }
-  },
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      "jose-elias-alvarez/null-ls.nvim",
-      config = function()
-        require "custom.configs.null-ls"
-      end,
-    },
-    config = function()
-      require "plugins.configs.lspconfig"
-      require "custom.configs.lspconfig"
-      require "custom.configs.null-ls"
-    end,
-  },
+	{ "christoomey/vim-tmux-navigator", lasy = false },
+	{
+		"williamboman/mason.nvim",
+		opts = {
+			ensure_installed = {
+				"dockerfile-language-server",
+				"eslint_d",
+				"lua-language-server",
+				"prettierd",
+				"typescript-language-server",
+				"clangd",
+				"clang-format",
+				"gopls",
+				"gofumpt",
+				"goimports-reviser",
+				"golines",
+				"rust-analyzer",
+				"rustfmt",
+				"stylua",
+			},
+		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			"jose-elias-alvarez/null-ls.nvim",
+			config = function()
+				require("custom.configs.null-ls")
+			end,
+		},
+		config = function()
+			require("plugins.configs.lspconfig")
+			require("custom.configs.lspconfig")
+			require("custom.configs.null-ls")
+		end,
+	},
 }
 
 return plugins
