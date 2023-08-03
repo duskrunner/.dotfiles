0bea2f5d9abd1cdfdcad50687c090ae0d37a719d
Stanislav Selin <stasvselin@gmail.com>
2023-08-03 23:50:50 +0300

    nv tree merge with nvchad conf

--- nvchad/.config/nvim/lua/custom/plugins.lua
+++ nvchad/.config/nvim/lua/custom/plugins.lua
@@ -124,55 +124,51 @@ local plugins = {
 	{ "christoomey/vim-tmux-navigator", lazy = false },
 	{
 		"nvim-tree/nvim-tree.lua",
-		config = function()
-			require("nvim-tree").setup({
-				hijack_cursor = true,
-				sync_root_with_cwd = true,
-				renderer = {
-					highlight_git = true,
-					full_name = true,
-					group_empty = true,
-					special_files = {},
-					symlink_destination = false,
-					indent_markers = {
-						enable = true,
-					},
-					icons = {
-						git_placement = "after",
-						show = {
-							file = true,
-							folder = true,
-							folder_arrow = false,
-							git = true,
-						},
-					},
-				},
-				update_focused_file = {
+		opts = {
+			hijack_cursor = true,
+			sync_root_with_cwd = true,
+			git = { enable = true },
+			renderer = {
+				highlight_git = true,
+				full_name = true,
+				group_empty = true,
+				special_files = {},
+				symlink_destination = false,
+				indent_markers = {
 					enable = true,
-					update_root = true,
-					ignore_list = { "help" },
 				},
-				diagnostics = {
-					enable = true,
-					show_on_dirs = true,
-				},
-				actions = {
-					change_dir = {
-						enable = false,
-						restrict_above_cwd = true,
-					},
-					open_file = {
-						resize_window = true,
-						window_picker = {
-							chars = "aoeui",
-						},
-					},
-					remove_file = {
-						close_window = false,
+				icons = {
+					git_placement = "after",
+					show = {
+						file = true,
+						folder = true,
+						folder_arrow = false,
+						git = true,
 					},
 				},
-			})
-		end,
+			},
+			update_focused_file = {
+				enable = true,
+				update_root = true,
+				ignore_list = { "help" },
+			},
+			diagnostics = {
+				enable = true,
+				show_on_dirs = true,
+			},
+			actions = {
+				change_dir = {
+					enable = false,
+					restrict_above_cwd = true,
+				},
+				open_file = {
+					resize_window = true,
+				},
+				remove_file = {
+					close_window = false,
+				},
+			},
+		},
 	},
 }
 
