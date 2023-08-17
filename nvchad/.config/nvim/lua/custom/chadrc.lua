bf2ac0fb38fae55342948a4eb6cf4655e9e4f19d
duskrunner <stasvselin@gmail.com>
2023-08-17 23:40:38 +0300

    basic wezterm setup

--- nvchad/.config/nvim/lua/custom/chadrc.lua
+++ nvchad/.config/nvim/lua/custom/chadrc.lua
@@ -1,6 +1,6 @@
 ---@type ChadrcConfig
 local M = {}
-M.ui = { theme = "catppuccin" }
+M.ui = { theme = "chadracula" }
 M.mappings = require("custom.mappings")
 M.plugins = "custom.plugins"
 return M
