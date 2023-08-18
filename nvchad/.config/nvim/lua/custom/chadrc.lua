4a8e4813e410a7ecdd01db86dded168eca954a69
duskrunner <stasvselin@gmail.com>
2023-08-18 19:43:02 +0300

    kitty as terminal

--- nvchad/.config/nvim/lua/custom/chadrc.lua
+++ nvchad/.config/nvim/lua/custom/chadrc.lua
@@ -3,4 +3,6 @@ local M = {}
 M.ui = { theme = "chadracula" }
 M.mappings = require("custom.mappings")
 M.plugins = "custom.plugins"
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 return M
