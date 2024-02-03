37ad2674d1043cbb011d26c7e22e8693ef5b47b3
duskrunner <stasvselin@gmail.com>
2024-02-03 20:50:26 +0300

    add venv support for mypy

--- nvchad/.config/nvim/lua/custom/configs/null-ls.lua
+++ nvchad/.config/nvim/lua/custom/configs/null-ls.lua
@@ -17,7 +17,12 @@ local sources = {
 	lint.shellcheck,
 	lint.eslint_d,
 	lint.ruff,
-	lint.mypy,
+	lint.mypy.with({
+		extra_args = function()
+			local virtual = os.getenv("VIRTUAL_ENV") or os.getenv("CONDA_PREFIX") or "/usr"
+			return { "--python-executable", virtual .. "/bin/python3" }
+		end,
+	}),
 }
 
 null_ls.setup({
