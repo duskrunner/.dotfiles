4903e2f8526f397c1566d283b1884c8d65c3156e
duskrunner <stasvselin@gmail.com>
2024-03-14 18:43:34 +0300

    feat: 🎸 conform config

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -282,26 +282,7 @@ require('lazy').setup {
   require 'plugins.mini.pairs',
   require 'plugins.mini.surround',
   require 'plugins.Comment_nvim',
-  {
-    'stevearc/conform.nvim',
-    opts = {
-      notify_on_error = false,
-      format_on_save = {
-        timeout_ms = 500,
-        lsp_fallback = true,
-      },
-      formatters_by_ft = {
-        lua = { 'stylua' },
-        -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
-        --
-        -- You can use a sub-list to tell conform to run *until* a formatter
-        -- is found.
-        -- javascript = { { "prettierd", "prettier" } },
-      },
-    },
-  },
-
+  require 'plugins.conform',
   require 'plugins.nvim-cmp',
   require 'plugins.LuaSnip',
 
---
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,31 @@
+return {
+  {
+    'stevearc/conform.nvim',
+    dependencies = { 'mason.nvim' },
+    opts = function()
+      ---@class ConformOpts
+      local opts = {
+        notify_on_error = false,
+        format_on_save = {
+          timeout_ms = 500,
+          lsp_fallback = true,
+        },
+        format = {
+          timeout_ms = 3000,
+          async = false, -- not recommended to change
+          quiet = false, -- not recommended to change
+          lsp_fallback = true, -- not recommended to change
+        },
+        formatters_by_ft = {
+          lua = { 'stylua' },
+          fish = { 'fish_indent' },
+          sh = { 'shfmt' },
+        },
+        formatters = {
+          injected = { options = { ignore_errors = true } },
+        },
+      }
+      return opts
+    end,
+  },
+}
