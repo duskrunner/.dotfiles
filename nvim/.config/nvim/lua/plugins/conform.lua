4af23ea4860b3ac87e31c39f1f3b9176dc2bdd56
duskrunner <stasvselin@gmail.com>
2024-03-15 19:24:28 +0300

    feat: 🎸 most plugins added

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -1,31 +1,28 @@
 return {
-  {
-    'stevearc/conform.nvim',
-    dependencies = { 'mason.nvim' },
-    opts = function()
-      ---@class ConformOpts
-      local opts = {
-        notify_on_error = false,
-        format_on_save = {
-          timeout_ms = 500,
-          lsp_fallback = true,
-        },
-        format = {
-          timeout_ms = 3000,
-          async = false, -- not recommended to change
-          quiet = false, -- not recommended to change
-          lsp_fallback = true, -- not recommended to change
-        },
-        formatters_by_ft = {
-          lua = { 'stylua' },
-          fish = { 'fish_indent' },
-          sh = { 'shfmt' },
-        },
-        formatters = {
-          injected = { options = { ignore_errors = true } },
-        },
-      }
-      return opts
-    end,
-  },
+  'stevearc/conform.nvim',
+  dependencies = { 'mason.nvim' },
+  opts = function()
+    ---@class ConformOpts
+    local opts = {
+      notify_on_error = false,
+      format_on_save = {
+        timeout_ms = 500,
+        lsp_fallback = true,
+      },
+      format = {
+        timeout_ms = 3000,
+        async = false, -- not recommended to change
+        quiet = false, -- not recommended to change
+        lsp_fallback = true, -- not recommended to change
+      },
+      formatters_by_ft = {
+        lua = { 'stylua' },
+        sh = { 'shfmt' },
+      },
+      formatters = {
+        injected = { options = { ignore_errors = true } },
+      },
+    }
+    return opts
+  end,
 }
