0cb31e5ab36b17a098174f4217b1e657d70528d4
duskrunner <stasvselin@gmail.com>
2024-04-13 22:38:12 +0300

    fix: 🐛 tsserver fix and doge

--- nvim/.config/nvim/lua/plugins/doge.lua
+++ nvim/.config/nvim/lua/plugins/doge.lua
@@ -1,4 +1,3 @@
 return {
   'kkoomen/vim-doge',
-  cmd = 'DogeGenerate',
 }
--- nvim/.config/nvim/lua/plugins/lsp/tsserver.lua
+++ nvim/.config/nvim/lua/plugins/lsp/tsserver.lua
@@ -1,32 +1,24 @@
+local map = require('utils').map
+
+map('n', '<leader>co', function()
+  vim.lsp.buf.code_action {
+    apply = true,
+    context = {
+      only = { 'source.organizeImports.ts' },
+      diagnostics = {},
+    },
+  }
+  map('n', '<leader>cR', function()
+    vim.lsp.buf.code_action {
+      apply = true,
+      context = {
+        only = { 'source.removeUnused.ts' },
+        diagnostics = {},
+      },
+    }
+  end, { desc = 'Remove unused imports' })
+end, { desc = 'Organize Imports' })
 return {
-  keys = {
-    {
-      '<leader>co',
-      function()
-        vim.lsp.buf.code_action {
-          apply = true,
-          context = {
-            only = { 'source.organizeImports.ts' },
-            diagnostics = {},
-          },
-        }
-      end,
-      desc = 'Organize Imports',
-    },
-    {
-      '<leader>cR',
-      function()
-        vim.lsp.buf.code_action {
-          apply = true,
-          context = {
-            only = { 'source.removeUnused.ts' },
-            diagnostics = {},
-          },
-        }
-      end,
-      desc = 'Remove Unused Imports',
-    },
-  },
   ---@diagnostic disable-next-line: missing-fields
   settings = {
     completions = {
