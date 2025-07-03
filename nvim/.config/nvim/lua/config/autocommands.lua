43bf846f4093c13701b047b162c22d6f06f3bb5b
duskrunner <stasvselin@gmail.com>
2025-07-03 17:23:50 +0300

    fix: 🐛 update deprecated methods

--- nvim/.config/nvim/lua/config/autocommands.lua
+++ nvim/.config/nvim/lua/config/autocommands.lua
@@ -12,10 +12,16 @@ vim.api.nvim_create_autocmd('User', {
 vim.api.nvim_create_autocmd('VimEnter', {
   desc = 'Set colorscheme',
   callback = function()
-    vim.fn.sign_define('DiagnosticSignError', { text = ' ', texthl = 'DiagnosticSignError' })
-    vim.fn.sign_define('DiagnosticSignWarn', { text = ' ', texthl = 'DiagnosticSignWarn' })
-    vim.fn.sign_define('DiagnosticSignInfo', { text = ' ', texthl = 'DiagnosticSignInfo' })
-    vim.fn.sign_define('DiagnosticSignHint', { text = '󰌵', texthl = 'DiagnosticSignHint' })
+    vim.diagnostic.config {
+      signs = {
+        text = {
+          [vim.diagnostic.severity.ERROR] = ' ',
+          [vim.diagnostic.severity.WARN] = ' ',
+          [vim.diagnostic.severity.INFO] = ' ',
+          [vim.diagnostic.severity.HINT] = '󰌵',
+        },
+      },
+    }
     require 'config.current-colorscheme'
   end,
 })
